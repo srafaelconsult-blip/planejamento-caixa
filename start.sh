f8c253c4c0347859ac8042c119e5fb2ce98f2236
srafaelconsult-blip <srafael.consult@gmail.com>
2025-08-31 18:04:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,12 @@ echo "=== INICIANDO APLICAÇÃO ==="
 
 # Criar tabelas do banco de dados
 echo "Criando tabelas do banco de dados..."
-python -c "from app import db, app; with app.app_context(): db.create_all()"
+python -c "
+from app import db, app
+with app.app_context():
+    db.create_all()
+    print('Tabelas criadas com sucesso!')
+"
 
 # Iniciar a aplicação
 echo "Iniciando servidor..."
